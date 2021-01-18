2532abd8712b273f59f85f0108f96d90a3e0f572
Antoine Bertin <ant.bertin@gmail.com>
2021-01-18 02:16:53 +0100

    Update

--- os/README.md
+++ os/README.md
@@ -5,7 +5,7 @@ minimum common image for my k3s nodes.
 The rest of the configuration is done by the `os` and `k3s` ansible roles.
 
 See various ways to achieve this for different architectures:
-* [arm64](arm64)
-* [amd64](amd64)
+* [arm64](arm64) with SD card image
+* [amd64](amd64) through PXE
 
 [1]: https://ubuntu.com/server/docs/install/autoinstall
--- os/amd64/prepare.sh
+++ os/amd64/prepare.sh
@@ -1,32 +1,45 @@
 #!/bin/bash
 set -euo pipefail
 
-# create output directories
-mkdir -p tftp
-mkdir -p www
-
-# download the iso
 url=https://releases.ubuntu.com/20.04/ubuntu-20.04.1-live-server-amd64.iso
 iso=$(basename $url)
- curl -sfLO --output-dir www/ \
-    $url
+echo "Preparing for $iso"
+
+# download the iso
+mkdir -p www
+if [ ! -f www/$iso ]; then
+    echo "Download ISO..."
+    curl -sfLO --output-dir www/ \
+        $url
+fi
 
 # copy kernel and initramfs from iso
-mkdir mnt
-sudo mount -o ro www/$iso mnt/
-cp -p mnt/casper/{vmlinuz,initrd} tftp/
-sudo umount mnt/
-rmdir mnt
+mkdir -p tftp
+if [ ! -f tftp/initrd ] || [ ! -f tftp/vmlinuz ]; then
+    echo "Copy kernel and initramfs..."
+    mkdir mnt
+    sudo mount -o ro www/$iso mnt/
+    [ -f tftp/vmlinuz ] || cp -p mnt/casper/vmlinuz tftp/
+    [ -f tftp/initrd ] || cp -p mnt/casper/initrd tftp/
+    sudo umount mnt/
+    rmdir mnt
+fi
 
 # download grub image
- curl -sfLo tftp/pxelinux.0 \
-    http://archive.ubuntu.com/ubuntu/dists/focal/main/uefi/grub2-amd64/current/grubnetx64.efi.signed
+if [ ! -f tftp/pxelinux.0 ]; then
+    echo "Download GRUB image..."
+    curl -sfLo tftp/pxelinux.0 \
+        http://archive.ubuntu.com/ubuntu/dists/focal/main/uefi/grub2-amd64/current/grubnetx64.efi.signed
+fi
 
 # copy grub configuration
+echo "Copy GRUB configuration..."
 mkdir -p tftp/grub
-cp grub.cfg tftp/grub/
+cp -f grub.cfg tftp/grub/
 
 # copy cloud-init files
-cp meta-data.yaml www/meta-data
-cp user-data.yaml www/user-data
+echo "Copy cloud-init files..."
+cp -f meta-data.yaml www/meta-data
+cp -f user-data.yaml www/user-data
 
+echo "Done!"
