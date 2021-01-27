3a2f85e37bc24dab8e4bf7f8ad1645de85a5f73c
Antoine Bertin <ant.bertin@gmail.com>
2021-01-27 21:43:49 +0100

    Update

--- hack/seal-secrets.sh
+++ hack/seal-secrets.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 for file in $(fd -t f -I '\-secret\.yaml'); do
-    kubeseal < "${file}" > "${file%-secret.yaml}-sealedsecret.yaml"
+    sealedfile="${file%-secret.yaml}-sealedsecret.yaml"
+    if [ ! -f "${sealedfile}" ]; then
+        echo "---" > "${sealedfile}"
+        kubeseal < "${file}" >> "${sealedfile}"
+    fi
 done
