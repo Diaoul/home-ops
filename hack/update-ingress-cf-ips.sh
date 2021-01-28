f473e9543f699e7f3afdc1cb8c340872460232c8
Antoine Bertin <ant.bertin@gmail.com>
2021-01-29 00:49:42 +0100

    Update

--- hack/update-ingress-cf-ips.sh
+++ hack/update-ingress-cf-ips.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+file="cluster/kube-system/ingress-nginx-external/helmrelease.yaml"
 ips=$( (\
     curl -s https://www.cloudflare.com/ips-v4 | sort; \
     curl -s https://www.cloudflare.com/ips-v6 | sort \
-    ) | cat | tr '\n' ' ')
+    ) | cat | tr '\n' ',' | sed -e 's/,$//' )
 
-yq e ".spec.values.controller.config.proxy-real-ip-cidr = \"$ips\"" \
-    cluster/kube-system/ingress-nginx-external/helmrelease.yaml
+# TODO re-add --- document start removed by yq
+# see https://github.com/mikefarah/yq/issues/351
+yq -i eval ".spec.values.controller.config.proxy-real-ip-cidr = \"$ips\"" $file
