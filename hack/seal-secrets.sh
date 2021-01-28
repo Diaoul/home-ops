a274885166801902b96dc1f89981aca48eb63e56
Antoine Bertin <ant.bertin@gmail.com>
2021-01-28 01:57:44 +0100

    Update

--- hack/seal-secrets.sh
+++ hack/seal-secrets.sh
@@ -3,6 +3,8 @@ for file in $(fd -t f -I '\-secret\.yaml'); do
     sealedfile="${file%-secret.yaml}-sealedsecret.yaml"
     if [ ! -f "${sealedfile}" ]; then
         echo "---" > "${sealedfile}"
-        kubeseal < "${file}" >> "${sealedfile}"
+        kubeseal < "${file}" |
+            yq eval 'del(.metadata.creationTimestamp)' - |
+            yq eval 'del(.spec.template.metadata.creationTimestamp)' - >> "${sealedfile}"
     fi
 done
