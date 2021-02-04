f12b0c2f48761e33ec558e257d39e9c099bee77b
Antoine Bertin <ant.bertin@gmail.com>
2021-02-04 19:12:26 +0100

    Update hack

---
+++ hack/create-helmrelease-annotations.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+shopt -s globstar
+
+# shellcheck disable=SC2155
+REPO_ROOT=$(git rev-parse --show-toplevel)
+CLUSTER_ROOT="${REPO_ROOT}/cluster"
+HELM_REPOSITORIES="${CLUSTER_ROOT}/flux-system/helm-chart-repositories"
+
+# Ensure yq exist
+command -v yq >/dev/null 2>&1 || {
+    echo >&2 "yq is not installed. Aborting."
+    exit 1
+}
+
+for helm_release in "${CLUSTER_ROOT}"/**/helmrelease.yaml; do
+    # ignore flux-system namespace
+    # ignore wrong apiVersion
+    # ignore non HelmReleases
+    if [[ "${helm_release}" =~ "flux-system"
+        || $(yq eval '.apiVersion' "${helm_release}") != "helm.toolkit.fluxcd.io/v2beta1"
+        || $(yq eval '.kind' "${helm_release}") != "HelmRelease" ]]; then
+        continue
+    fi
+
+    for helm_repository in "${HELM_REPOSITORIES}"/*.yaml; do
+        chart_name=$(yq eval '.metadata.name' "${helm_repository}")
+        chart_url=$(yq eval '.spec.url' "${helm_repository}")
+
+        # only helmreleases where helm_release is related to chart_url
+        if [[ $(yq eval '.spec.chart.spec.sourceRef.name' "${helm_release}") == "${chart_name}" ]]; then
+            name="$(basename $(dirname $(dirname ${helm_release})))/$(basename $(dirname ${helm_release}))"
+            echo "Annotating ${name} with ${chart_name}..."
+            # delete "renovate: registryUrl=" line
+            sed -i "/renovate: registryUrl=/d" "${helm_release}"
+            # insert "renovate: registryUrl=" line
+            sed -i "/.*chart: .*/i\ \ \ \ \ \ # renovate: registryUrl=${chart_url}" "${helm_release}"
+            break
+        fi
+    done
+done
--- hack/seal-secrets.sh
+++ hack/seal-secrets.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-for file in $(fd -t f -I '\-secret\.yaml'); do
+root=$(git rev-parse --show-toplevel)
+for file in $(fd -t f -I '\-secret\.yaml' "$root"); do
     sealedfile="${file%-secret.yaml}-sealedsecret.yaml"
     if [ ! -f "${sealedfile}" ]; then
+        echo "Creating $sealedfile..."
         echo "---" > "${sealedfile}"
         kubeseal < "${file}" |
             yq eval 'del(.metadata.creationTimestamp)' - |
