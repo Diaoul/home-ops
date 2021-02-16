10b58baf9e94893fc00012d84120338245e0ead1
Antoine Bertin <ant.bertin@gmail.com>
2021-02-16 23:21:17 +0100

    fix(hack): no kustomize if no kustomization

--- hack/kustomize.sh
+++ hack/kustomize.sh
@@ -4,5 +4,9 @@ files=$(git diff --staged --name-only | grep "^cluster/")
 lcp=$(echo "$files" | sed -e 'N;s/^\(.*\).*\n\1.*$/\1\n\1/;D')
 parent=$(echo "$lcp" | sed 's/\(.*\)\/.*/\1/')
 
-echo "Running kustomize build $parent..."
-kustomize build $parent > /dev/null
+if [ -f "$parent/kustomization.yaml" ]; then
+    echo "Running kustomize build $parent..."
+    kustomize build "$parent" > /dev/null
+else
+    echo "No kustomization.yaml in $parent"
+fi
