ff13f57f953070fdb81e436df532ae18b128d16b
Antoine Bertin <ant.bertin@gmail.com>
2021-04-22 22:51:29 +0200

    fix(hack): cf ips file location

--- hack/update-ingress-cf-ips.sh
+++ hack/update-ingress-cf-ips.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 root=$(git rev-parse --show-toplevel)
-file="$root/cluster/network-system/ingress-nginx/helmrelease.yaml"
+file="$root/cluster/apps/networking/ingress-nginx/helmrelease.yaml"
 ips=$( (\
     curl -s https://www.cloudflare.com/ips-v4 | sort; \
     curl -s https://www.cloudflare.com/ips-v6 | sort \
