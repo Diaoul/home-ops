47d65bd5aeed4ea62bb82756df6ea91c7a6589b9
Antoine Bertin <antoine@bertin.io>
2022-08-29 08:57:32 +0200

    feat: add script to apply kyverno policies

---
+++ hack/kyverno-apply.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+workdir=$(mktemp -d)
+echo "Created workdir $workdir"
+for clusterpolicy in $(kubectl get clusterpolicies -A -o name)
+do
+    policy_workdir=$workdir/${clusterpolicy##*/}
+    mkdir $policy_workdir
+    kubectl get $clusterpolicy -o yaml > $policy_workdir/clusterpolicy.yaml
+    kyverno apply $policy_workdir/clusterpolicy.yaml --cluster --output $policy_workdir/mutated
+    kubectl apply -f $policy_workdir/mutated
+done
