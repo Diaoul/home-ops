51bce75795ee358ef3bc9b47a3687c45067e87f3
Antoine Bertin <antoine@bertin.io>
2024-06-20 23:18:36 +0200

    feat!: migrate to new cluster

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -16,16 +16,16 @@ kubeconform_args=(
   "-verbose"
 )
 
-echo "=== Validating standalone manifests in ./cluster/flux ==="
-find "./cluster/flux" -maxdepth 1 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
+echo "=== Validating standalone manifests in ./kubernetes/flux ==="
+find "./kubernetes/flux" -maxdepth 1 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
   kubeconform "${kubeconform_args[@]}" "${file}"
   if [[ ${PIPESTATUS[0]} != 0 ]]; then
     exit 1
   fi
 done
 
-echo "=== Validating kustomizations in ./cluster/flux ==="
-find "./cluster/flux" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
+echo "=== Validating kustomizations in ./kubernetes/flux ==="
+find "./kubernetes/flux" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
   echo "=== Validating kustomizations in ${file/%$kustomize_config/} ==="
   kustomize build "${file/%$kustomize_config/}" "${kustomize_args[@]}" |
     kubeconform "${kubeconform_args[@]}"
@@ -34,8 +34,8 @@ find "./cluster/flux" -type f -name $kustomize_config -print0 | while IFS= read 
   fi
 done
 
-echo "=== Validating kustomizations in ./cluster/apps ==="
-find "./cluster/apps" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
+echo "=== Validating kustomizations in ./kubernetes/apps ==="
+find "./kubernetes/apps" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
   echo "=== Validating kustomizations in ${file/%$kustomize_config/} ==="
   kustomize build "${file/%$kustomize_config/}" "${kustomize_args[@]}" |
     kubeconform "${kubeconform_args[@]}"
