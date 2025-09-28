1524241a8915a045e59f274095eb701752d0fbed
Antoine Bertin <antoine@bertin.io>
2025-09-28 12:29:11 +0200

    feat(github): rework workflows

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -1,45 +1,52 @@
 #!/usr/bin/env bash
-set -o errexit
-set -o pipefail
+
+set -euo pipefail
+
+KUBERNETES_DIR=$1
+
+[[ -z "${KUBERNETES_DIR}" ]] && echo "Kubernetes location not specified" && exit 1
 
 kustomize_args=("--load-restrictor=LoadRestrictionsNone")
 kustomize_config="kustomization.yaml"
 kubeconform_args=(
-  "-strict"
-  "-ignore-missing-schemas"
-  "-skip"
-  "Secret,ReplicationDestination,ReplicationSource"
-  "-schema-location"
-  "default"
-  "-schema-location"
-  "https://kubernetes-schemas.pages.dev/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json"
-  "-verbose"
+    "-strict"
+    "-ignore-missing-schemas"
+    "-skip"
+    "Gateway,HTTPRoute,Secret,ReplicationDestination,ReplicationSource"
+    "-schema-location"
+    "default"
+    "-schema-location"
+    "https://kubernetes-schemas.pages.dev/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json"
+    "-schema-location"
+    "https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json"
+    "-verbose"
 )
 
-echo "=== Validating standalone manifests in ./kubernetes/flux ==="
-find "./kubernetes/flux" -maxdepth 1 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
-  kubeconform "${kubeconform_args[@]}" "${file}"
-  if [[ ${PIPESTATUS[0]} != 0 ]]; then
-    exit 1
-  fi
+echo "=== Validating standalone manifests in ${KUBERNETES_DIR}/flux ==="
+find "${KUBERNETES_DIR}/flux" -maxdepth 1 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
+do
+    kubeconform "${kubeconform_args[@]}" "${file}"
+    if [[ ${PIPESTATUS[0]} != 0 ]]; then
+        exit 1
+    fi
 done
 
-echo "=== Validating kustomizations in ./kubernetes/flux ==="
-find "./kubernetes/flux" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
-  echo "=== Validating kustomizations in ${file/%$kustomize_config/} ==="
-  kustomize build "${file/%$kustomize_config/}" "${kustomize_args[@]}" |
-    kubeconform "${kubeconform_args[@]}"
-  if [[ ${PIPESTATUS[0]} != 0 ]]; then
-    exit 1
-  fi
+echo "=== Validating kustomizations in ${KUBERNETES_DIR}/flux ==="
+find "${KUBERNETES_DIR}/flux" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
+do
+    echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
+    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | kubeconform "${kubeconform_args[@]}"
+    if [[ ${PIPESTATUS[0]} != 0 ]]; then
+        exit 1
+    fi
 done
 
-echo "=== Validating kustomizations in ./kubernetes/apps ==="
-find "./kubernetes/apps" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
-  echo "=== Validating kustomizations in ${file/%$kustomize_config/} ==="
-  kustomize build "${file/%$kustomize_config/}" "${kustomize_args[@]}" |
-    kubeconform "${kubeconform_args[@]}"
-  if [[ ${PIPESTATUS[0]} != 0 ]]; then
-    exit 1
-  fi
+echo "=== Validating kustomizations in ${KUBERNETES_DIR}/apps ==="
+find "${KUBERNETES_DIR}/apps" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
+do
+    echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
+    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | kubeconform "${kubeconform_args[@]}"
+    if [[ ${PIPESTATUS[0]} != 0 ]]; then
+        exit 1
+    fi
 done
